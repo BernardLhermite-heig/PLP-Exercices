a3dd81f8cbbc3e90ef076aaa8881f4d3d508bdb9
BernardLhermite <stephane.marengo@hotmail.com>
2022-03-20 13:52:55 +0100

    devoirs - roman

---
+++ devoirs/01/roman.hs
@@ -0,0 +1,34 @@
+module Roman (toRoman, fromRoman) where
+
+romanMap = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+value 'I' = 1
+value 'V' = 5
+value 'X' = 10
+value 'L' = 50
+value 'C' = 100
+value 'D' = 500
+value 'M' = 1000
+
+toRoman :: Int -> Maybe String
+toRoman x
+  | x > 0 && x <= 3999 = Just $ snd $ foldl f (x, "") romanMap
+  | otherwise = Nothing
+  where
+    f (n, str) (chiffre, lettre) = (l, str ++ concat (replicate k lettre))
+      where
+        (k, l) = divMod n chiffre
+
+fromRoman :: String -> Maybe Int
+fromRoman str = if Just str == s then Just n else Nothing
+  where
+    n = convert str
+    s = toRoman n
+    convert [] = 0
+    convert [s] = value s
+    convert (s1 : s2 : str)
+      | n1 < n2 = n2 - n1 + convert str
+      | otherwise = n1 + convert (s2 : str)
+      where
+        n1 = value s1
+        n2 = value s2

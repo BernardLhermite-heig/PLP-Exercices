f8a51f57d7073659521a8e299685960406d2adcd
BernardLhermite <stephane.marengo@hotmail.com>
2022-04-27 21:46:34 +0200

    Refactor river

--- devoirs/01/river.hs
+++ devoirs/01/river.hs
@@ -10,7 +10,7 @@ import System.Environment
 import System.Exit (exitSuccess)
 import System.IO
 
-data Character = Farmer | Wolf | Goat | Cabbage
+data Character = Wolf | Goat | Cabbage
   deriving (Show, Eq)
 
 data Side = LeftBank | RightBank
@@ -19,7 +19,7 @@ data Boat = Boat Character | EmptyBoat
 
 data State = State Side Boat [Character] [Character]
 
-data Move = ValidMove State | InvalidMove String
+data Move = ValidMove State | InvalidMove State String
 
 river :: String
 river = replicate 40 '~'
@@ -28,8 +28,8 @@ unknownCommand :: String
 unknownCommand = "Commande inconnue"
 
 instance Show Boat where
-  show (Boat passenger) = "Barque: " ++ show Farmer ++ ", " ++ show passenger
-  show EmptyBoat = "Barque: " ++ show Farmer
+  show (Boat passenger) = show EmptyBoat ++ ", " ++ show passenger
+  show EmptyBoat = "Barque: Farmer"
 
 instance Show State where
   show (State side boat left right) = showLeft ++ showRiver ++ showRight
@@ -44,59 +44,55 @@ main :: IO ()
 main =
   do
     help
-    let initialState = initGame
-    print initialState
-    gameLoop initialState
+    let initialMove@(ValidMove state) = initGame
+    print state
+    gameLoop initialMove
 
 help :: IO ()
 help = do
   putStrLn ":p afficher l'état du jeu"
-  putStrLn ":l <passenger> charger la barque avec un passager"
+  putStrLn ":l <passenger> charger la barque avec un passager (ex: :l wolf)"
   putStrLn ":u décharger la barque"
   putStrLn ":m déplacer la barque"
   putStrLn ":r réinitialiser le jeu"
   putStrLn ":q quitter le jeu"
   putStrLn ":h afficher l'aide"
 
-quit :: IO a
+quit :: IO ()
 quit = exitSuccess
 
-initGame :: State
-initGame = State LeftBank EmptyBoat [Wolf, Goat, Cabbage] []
+initGame :: Move
+initGame = ValidMove $ State LeftBank EmptyBoat [Wolf, Goat, Cabbage] []
 
-gameLoop :: State -> IO ()
-gameLoop state
+gameLoop :: Move -> IO ()
+gameLoop (InvalidMove oldState msg) = putStrLn msg >> gameLoop (ValidMove oldState)
+gameLoop move@(ValidMove state)
   | hasWon state = putStrLn "Vous avez gagné!" >> askReplay
   | otherwise = do
     putStr "> "
     hFlush stdout
     cmd <- getLine
-    nextState <- parseCmd cmd
-    gameLoop nextState
+    case parseCmd (map toLower cmd) of
+      Left move -> gameLoop move
+      Right msg -> msg >> gameLoop (ValidMove state)
   where
+    parseCmd :: String -> Either Move (IO ())
     parseCmd (':' : cmd : rest) =
       case cmd of
-        'p' -> print state >> return state
+        'p' -> Right $ print state
         'l' -> case rest of
-          (' ' : arg) ->
+          (' ' : arg) -> Left $
             case parseArg arg of
-              Just passenger ->
-                case loadPassenger state passenger of
-                  ValidMove nextMove -> return nextMove
-                  InvalidMove msg -> putStrLn msg >> return state
-              Nothing -> putStrLn "Passager inconnu" >> return state
-          _ -> putStrLn "Passager manquant" >> return state
-        'u' -> case unloadPassenger state of
-          ValidMove nextMove -> return nextMove
-          InvalidMove msg -> putStrLn msg >> return state
-        'm' -> case moveBoat state of
-          ValidMove nextMove -> return nextMove
-          InvalidMove msg -> putStrLn msg >> return state
-        'r' -> return initGame
-        'q' -> quit
-        'h' -> help >> return state
-        _ -> putStrLn unknownCommand >> return state
-    parseCmd _ = putStrLn unknownCommand >> return state
+              Just passenger -> loadPassenger state passenger
+              Nothing -> InvalidMove state "Passager inconnu"
+          _ -> Left $ InvalidMove state "Passager manquant"
+        'u' -> Left $ unloadPassenger state
+        'm' -> Left $ moveBoat state
+        'r' -> Left initGame
+        'q' -> Right quit
+        'h' -> Right help
+        _ -> Left $ InvalidMove state unknownCommand
+    parseCmd _ = Left $ InvalidMove state unknownCommand
 
 askReplay :: IO ()
 askReplay = do
@@ -108,10 +104,10 @@ askReplay = do
     _ -> putStrLn "Réponse invalide" >> askReplay
 
 validateMove :: State -> Move
-validateMove state@(State side _ left right)
-  | hasWolf && hasGoat = InvalidMove "Le loup ne peut pas être laissé seul avec la chèvre"
-  | hasGoat && hasCabbage = InvalidMove "La chèvre ne peut pas être laissée seule avec les choux"
-  | otherwise = ValidMove state
+validateMove move@(State side _ left right)
+  | hasWolf && hasGoat = InvalidMove move "Le loup ne peut pas être laissé seul avec la chèvre"
+  | hasGoat && hasCabbage = InvalidMove move "La chèvre ne peut pas être laissée seule avec les choux"
+  | otherwise = ValidMove move
   where
     bank = case side of
       LeftBank -> right
@@ -129,21 +125,21 @@ moveBoat (State side boat left right) = validateMove nextMove
     nextMove = State oppositeSide boat left right
 
 loadPassenger :: State -> Character -> Move
-loadPassenger (State side boat left right) passenger =
+loadPassenger move@(State side boat left right) passenger =
   case boat of
-    Boat _ -> InvalidMove "La barque contient déjà un passager"
+    Boat _ -> InvalidMove move "La barque contient déjà un passager"
     EmptyBoat
       | isValid -> ValidMove $ State side (Boat passenger) newLeft newRight
-      | otherwise -> InvalidMove "Ce passager se trouve sur l'autre rive"
+      | otherwise -> InvalidMove move "Ce passager se trouve sur l'autre rive"
       where
         (isValid, newLeft, newRight) = case side of
           LeftBank -> (passenger `elem` left, delete passenger left, right)
           RightBank -> (passenger `elem` right, left, delete passenger right)
 
 unloadPassenger :: State -> Move
-unloadPassenger (State side boat left right) =
+unloadPassenger move@(State side boat left right) =
   case boat of
-    EmptyBoat -> InvalidMove "La barque est vide"
+    EmptyBoat -> InvalidMove move "La barque est vide"
     Boat passenger -> ValidMove $ State side EmptyBoat newLeft newRight
       where
         (newLeft, newRight) = case side of

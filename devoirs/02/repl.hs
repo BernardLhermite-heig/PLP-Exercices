989d58037273f0d389cb8c6083c15175530f9c12
BernardLhermite <stephane.marengo@hotmail.com>
2022-06-13 21:02:30 +0200

    :f

--- devoirs/02/repl.hs
+++ devoirs/02/repl.hs
@@ -6,11 +6,17 @@ import Language
 import Lexer
 import Parser
 import Semantics
+import System.Directory (doesFileExist)
 import System.Exit (exitSuccess)
 import System.IO
 import Text.Printf (printf)
 
-data State = StandardState TEnv Env | MessageState TEnv Env String | QuitState | EditionState TEnv Env
+data State
+  = StandardState TEnv Env
+  | MessageState TEnv Env String
+  | QuitState
+  | EditionState TEnv Env
+  | FromFileState TEnv Env String
 
 main :: IO ()
 main =
@@ -25,6 +31,7 @@ help =
     ++ "\n:r réinitialiser l'état de l'interpréteur"
     ++ "\n:t <expr> afficher le type d'une expression"
     ++ "\n:e afficher l'environnement"
+    ++ "\n:f <fichier> charger un fichier"
     ++ "\n:h afficher l'aide"
     ++ "\n:q quitter le programme"
 
@@ -38,18 +45,18 @@ displayMessage msg = catch (putStrLn msg) handler -- catch nécessaire si c'est 
 
 repl :: State -> IO ()
 repl QuitState = quit
+repl state@(FromFileState tEnv env file) = do
+  fileExists <- doesFileExist file
+  if not fileExists
+    then repl (MessageState tEnv env ("Le fichier '" ++ file ++ "' n'existe pas"))
+    else do
+      content <- readFile file
+      state <- evalLines (lines content) tEnv env state
+      repl state
 repl state@(EditionState tEnv env) = do
   content <- readUntil (== ":}")
   state <- evalLines content tEnv env state
   repl state
-  where
-    evalLines [] tEnv env state = return state
-    evalLines (l : ls) tEnv env state = do
-      state <- evalLine l tEnv env
-      case state of
-        StandardState tEnv' env' -> evalLines ls tEnv' env' state
-        MessageState tEnv' env' msg -> displayMessage msg >> evalLines ls tEnv' env' (StandardState tEnv' env')
-        _ -> error "this should not happen"
 repl (MessageState oldTEnv oldEnv msg) = displayMessage msg >> repl (StandardState oldTEnv oldEnv)
 repl state@(StandardState tEnv env) =
   do
@@ -86,6 +93,9 @@ parseCmd cmd rest tEnv env state =
       (' ' : arg) -> MessageState tEnv env (show $ fst $ typeof (parseLine arg) tEnv)
       _ -> MessageState tEnv env "Missing argument <expr>"
     'e' -> MessageState tEnv env (show tEnv ++ "\n" ++ show env)
+    'f' -> case rest of
+      (' ' : arg) -> FromFileState tEnv env arg
+      _ -> MessageState tEnv env "Missing argument <fichier>"
     'h' -> MessageState tEnv env help
     'q' -> QuitState
     _ -> MessageState tEnv env "Unknown command"
@@ -102,3 +112,11 @@ collectUntil act f = do
 
 readUntil :: (String -> Bool) -> IO [String]
 readUntil = collectUntil getLine
+
+evalLines [] tEnv env state = return state
+evalLines (l : ls) tEnv env state = do
+  state <- evalLine l tEnv env
+  case state of
+    StandardState tEnv' env' -> evalLines ls tEnv' env' state
+    MessageState tEnv' env' msg -> displayMessage msg >> evalLines ls tEnv' env' (StandardState tEnv' env')
+    _ -> error "this should not happen"
